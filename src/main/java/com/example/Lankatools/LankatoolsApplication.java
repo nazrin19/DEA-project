b79254d01f4d9c9b36a4d6ab578a361b518d9334
nazrin19 <nazreenabanu85@gmail.com>
2026-05-15 14:21:30 +0530

    initial project setup

---
+++ src/main/java/com/example/Lankatools/LankatoolsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Lankatools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LankatoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LankatoolsApplication.class, args);
+	}
+
+}
